beabd930906856289e5747e3c4c13c4cae02a71e
SergioCikilot <62554373+SergioCikilot@users.noreply.github.com>
2022-07-22 22:02:27 +0300

    fix

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,6 +1,6 @@
-var { execSync } = require("child_process");
-var path = require("path");
-var fs = require("fs");
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
